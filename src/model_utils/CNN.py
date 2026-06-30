545b8d232f3b32d77ab405b245a0eae888ee3cd1
feprevot <felixprevot@gmail.com>
2026-06-30 18:34:09 +0200

    fix: rm BatchNorm layers from CNN model and update model file 96 hit

--- src/model_utils/CNN.py
+++ src/model_utils/CNN.py
@@ -15,9 +15,9 @@ class LeafCNN(nn.Module):
         self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)  # ->128
         self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding=1)  # ->256
         # BatchNorm : normalise les sorties de conv -> entraînement stable.
-        self.bn1 = nn.BatchNorm2d(64)
-        self.bn2 = nn.BatchNorm2d(128)
-        self.bn3 = nn.BatchNorm2d(256)
+        #self.bn1 = nn.BatchNorm2d(64)
+        #self.bn2 = nn.BatchNorm2d(128)
+        #self.bn3 = nn.BatchNorm2d(256)
         # MaxPool : divise la taille de l'image par 2 (résume l'info).
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
         # ReLU : non-linéarité (met les valeurs négatives à 0).
@@ -29,15 +29,15 @@ class LeafCNN(nn.Module):
 
     def forward(self, x):
         x = self.conv1(x)
-        x = self.bn1(x)
+        #x = self.bn1(x)
         x = self.relu(x)
         x = self.pool(x)
         x = self.conv2(x)
-        x = self.bn2(x)
+        #x = self.bn2(x)
         x = self.relu(x)
         x = self.pool(x)
         x = self.conv3(x)
-        x = self.bn3(x)
+        #x = self.bn3(x)
         x = self.relu(x)
         x = self.gap(x)
         x = x.view(x.size(0), -1)
