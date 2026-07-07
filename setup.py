dcb9b9d0d79be4bb3febb9d7ffce60fb0e369f13
feprevot <felixprevot@gmail.com>
2026-07-07 10:29:26 +0200

    feat: - externalize zip - externalize mask_dataset - clear code - add auto train dataset processing (augmentation + masking) - prepare for evaluation

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ import argparse
 import os
 
 from srcs import Distribution, Augmentation
-from srcs.classification import train, predict, evaluate
+from srcs.classification import (
+    train, predict, evaluate, mask_dataset, zip
+)
 from srcs.utils import error_exit
 
 DEFAULT_MODEL = os.path.join("models", "model.pth")
@@ -29,6 +31,14 @@ def build_parser():
     p_eval = sub.add_parser("evaluate", help="evaluate the model")
     p_eval.add_argument("zip", nargs="?", default=DEFAULT_ZIP)
 
+    p_mask = sub.add_parser("mask", help="generate a masked dataset")
+    p_mask.add_argument("src", help="source dataset directory")
+    p_mask.add_argument("dst", help="output masked dataset directory")
+
+    p_pkg = sub.add_parser("zip", help="zip model + dataset + signature")
+    p_pkg.add_argument("directory", help="dataset to include in the zip")
+    p_pkg.add_argument("model", nargs="?", default=DEFAULT_MODEL)
+
     return parser
 
 
@@ -49,6 +59,10 @@ def main():
         if not os.path.isfile(args.zip):
             error_exit(f"'{args.zip}' is not a valid file")
         evaluate.run(args.zip)
+    elif args.command == "mask":
+        mask_dataset.run(args.src, args.dst)
+    elif args.command == "package":
+        zip.run(args.model, args.directory)
 
 
 if __name__ == "__main__":
